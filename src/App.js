3dc09aa5db4ab96eff536b8b5d019156aad2437e
AlexnFokin <ax9642824@gmail.com>
2022-09-08 18:33:19 +0300

    added fetch service

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import './styles/App.css'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import PostList from "./components/PostList";
 import PostForm from "./components/UI/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import MyLoader from "./components/UI/Loader/MyLoader";
+import {useFetching} from "./hooks/useFetching";
 
 
 function App() {
 
   const [posts, setPosts] = useState([])
-
-
   const [modal, setModal] = useState('false')
+  const [filter, setFilter] = useState({sort: '', query: ''})
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
+  const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll()
+    setPosts(posts)
+  })
+
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
     setModal('false')
@@ -23,8 +37,6 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-const [filter, setFilter] = useState({sort: '', query: ''})
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   return (
     <div className='App'>
@@ -46,13 +58,18 @@ const [filter, setFilter] = useState({sort: '', query: ''})
         setFilter={setFilter}
       />
       <hr style={{margin: '15px auto'}}/>
-
-     <PostList
+      {postError &&
+      <h1>Error ${postError}</h1>
+      }
+      {isPostLoading
+        ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><MyLoader/></div>
+        :
+        <PostList
           posts={sortedAndSearchedPosts}
           remove={removePost}
           title='Post list'
-     />
-
+        />
+      }
 
     </div>
 
